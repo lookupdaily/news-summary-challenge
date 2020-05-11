d0be60b595a745bb87f22c7d0a0eafbe2f7fa08a
hansa-lallu <hansalallu@gmail.com>
2020-05-11 11:10:04 +0100

    request for API

--- src/article.js
+++ src/article.js
@@ -1,8 +1,24 @@
-function Article() {
-  this.articles = getHttp.makeRequest("http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/football");
-  console.log(this.articles)
+var xhr = new XMLHttpRequest();
+var data 
+var articles = document.getElementById("articles")
+
+xhr.onload = function () {
+  if (xhr.status <= 200 && xhr.status < 300) {
+    data = JSON.parse(this.response)
+    articles.innerHTML = getArticles()
+  } else {
+    console.log('The request failed!')
+  }
 };
 
-Article.prototype.fetchArticle() {
-  
-}
+xhr.open('GET', 'http://news-summary-api.herokuapp.com/guardian?apiRequestUrl=http://content.guardianapis.com/football');
+xhr.send();
+
+var getArticles = function() {
+  var string = ""
+  data.response.results.forEach(article => { 
+    string += "<p>" + article.webTitle + "</p>"
+  });
+
+  return string
+};
