180412ad29382f6fda867a3ed74f70f5d0bd0093
hansa-lallu <hansalallu@gmail.com>
2020-06-19 11:26:48 +0100

    it can create an article

--- spec/articleListModelSpec.js
+++ spec/articleListModelSpec.js
@@ -1,11 +1,16 @@
 describe('articleListModel', () => {
   const data = { response: {
   results: [{ webTitle: 'webTitle', webUrl: 'webUrl' }]
-  }}
-  console.log(data)
+  }};
 
   it('is created with no articles', () => {
     const articleList = new ArticleListModel()
     expect(articleList.articles).toEqual([])
-  })
+  });
+
+  it('creates an article', () => {
+    const articleList = new ArticleListModel()
+    articleList.createArticles(data)
+    expect(articleList.articles.length).toEqual(1)
+  });
 });
