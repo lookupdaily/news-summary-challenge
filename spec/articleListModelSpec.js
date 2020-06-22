95f440224d272a2290775e48590bedf574a73633
Liz Daly <liz7daly@gmail.com>
2020-06-22 10:08:56 +0100

    Create spy for articleModel in tests

--- spec/articleListModelSpec.js
+++ spec/articleListModelSpec.js
@@ -1,7 +1,13 @@
 describe('articleListModel', () => {
-  const data = { response: {
-  results: [{ webTitle: 'webTitle', webUrl: 'webUrl' }]
-  }};
+  let ArticleModelSpy
+  let data
+
+  beforeEach(() => {
+    ArticleModelSpy = jasmine.createSpy('ArticleModelSpy')
+    data = { response: {
+      results: [{ webTitle: 'webTitle', webUrl: 'webUrl' }]
+    }};
+  })
 
   it('is created with no articles', () => {
     const articleList = new ArticleListModel()
@@ -12,5 +18,11 @@ describe('articleListModel', () => {
     const articleList = new ArticleListModel()
     articleList.createArticles(data)
     expect(articleList.articles.length).toEqual(1)
+  });
+
+  it('new article is an instance of article', () => {
+    const articleList = new ArticleListModel(ArticleModelSpy)
+    articleList.createArticles(data)
+    expect(articleList.articles[0]).toEqual(jasmine.any(ArticleModelSpy))
   });
 });
--- src/articleListModel.js
+++ src/articleListModel.js
@@ -1,9 +1,10 @@
-function ArticleListModel() {
+function ArticleListModel(articleModel = ArticleModel) {
   this.articles = []
+  this.articleModel = articleModel
 };
 
 ArticleListModel.prototype.createArticles = function(data) {
   data.response.results.forEach(item => { 
-    this.articles.push(new ArticleModel(item.webTitle, item.webUrl))
+    this.articles.push(new this.articleModel(item.webTitle, item.webUrl))
   });
 };
