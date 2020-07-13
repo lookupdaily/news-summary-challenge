c8e0b6f3a04047866c3140d23125b6e7aa6a17d3
Liz Daly <liz7daly@gmail.com>
2020-07-13 10:53:50 +0100

    Add React scripts

--- src/articleListView.js
+++ src/articleListView.js
@@ -3,8 +3,6 @@ function ArticleListView(articleList) {
 }
 
 ArticleListView.prototype.showArticles = function(){
-  console.log(this.articleList.getArticles())
-  console.log(this.articleList)
   var string = "<ul>"
   this.articleList.getArticles().forEach(article => { 
     string += `<li><a href="${article.getWebUrl()}">` + article.getTitle() + "</a></li>"
